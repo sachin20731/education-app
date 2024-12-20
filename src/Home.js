0d03883b6d50126d617b08ced6511b20e44258c4
sachin20731 <munasinghe20731@gmail.com>
2024-12-20 11:09:57 +0530

    Create Home page

---
+++ src/Home.js
@@ -0,0 +1,29 @@
+import React from "react";
+import LoginForm from "./LoginForm";
+import "./Home.css";
+import background from "./annie-spratt-OIuCXxx08yg-unsplash.jpg";
+import logo from "./file.png";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+    
+
+      <div className="login-box">
+        <LoginForm />
+        
+      </div>
+      <div className="css">
+      <img src={logo} alt="G+ Education Logo" className="logo" /> 
+        
+      
+      </div>
+      <div className="ss-home">
+          <h1 className="ss1"> Welcome To <br/> <span className="ss">  G+ Education</span></h1>
+        </div>
+        <div className="ss-head"> G+ Education Is One oF The Best Online learning Platform</div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/LoginForm.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+const LoginForm = () => {
+  const [formData, setFormData] = useState({ username: "", password: "" });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Username: ${formData.username}, Password: ${formData.password}`);
+  };
+
+  return (
+    <form className="login-form" onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <div className="form-group">
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default LoginForm;
